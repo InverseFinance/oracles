50b200ca665a4249a181a6fd3683a246b4c82aec
08xmt <markus.08xmt@gmail.com>
2022-05-06 21:13:37 +0200

    Add gas improvements

--- src/UniswapTwapPriceOracleV2Ceiling.sol
+++ src/UniswapTwapPriceOracleV2Ceiling.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.12;
 
-import "../lib/openzeppelin-contracts-upgradeable/contracts/math/SafeMathUpgradeable.sol";
-import "../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "../lib/openzeppelin-contracts/contracts/math/SafeMath.sol";
 
 import "src/IUniswapV2Pair.sol";
 
@@ -12,7 +11,7 @@ import "src/IUniswapV2Pair.sol";
  * @notice Stores cumulative prices and returns TWAPs for assets on Uniswap V2 pairs.
  */
 contract UniswapTwapPriceOracleV2Ceiling {
-    using SafeMathUpgradeable for uint256;
+    using SafeMath for uint256;
 
     /**
      * @dev Current price ceiling for the oracle
@@ -42,7 +41,7 @@ contract UniswapTwapPriceOracleV2Ceiling {
     /**
      * @dev uniswapV2 pair between underlying and WETH
      */
-    address immutable public pair;
+    IUniswapV2Pair public pair;
 
     /**
      * @dev Governance address, can set maxBPCeiling and minBPCeiling
@@ -57,16 +56,24 @@ contract UniswapTwapPriceOracleV2Ceiling {
     /**
      * @dev Minimum TWAP interval.
      */
-    uint256 public immutable MIN_TWAP_TIME;
+    uint256 immutable public MIN_TWAP_TIME;
 
-    constructor(uint MIN_TWAP_TIME_, uint maxBPCeiling_, uint minBPCeiling_, address pair_, address governance_, address guardian_) public{
+    /**
+     * @dev Internal baseUnit used as mantissa, set from decimals of underlying.
+     */
+    uint immutable private baseUnit;
+
+    constructor(uint MIN_TWAP_TIME_, uint maxBPCeiling_, uint minBPCeiling_, uint underlyingDecimals, address pair_, address governance_, address guardian_) public{
         MIN_TWAP_TIME = MIN_TWAP_TIME_;
         maxBPCeiling = maxBPCeiling_;
         minBPCeiling = minBPCeiling_;
         governance = governance_;
         guardian = guardian_;
-        pair = pair_;
+        pair = IUniswapV2Pair(pair_);
         underlying = IUniswapV2Pair(pair_).token0() == WETH ? IUniswapV2Pair(pair_).token1() : IUniswapV2Pair(pair_).token0();
+        baseUnit = 10 ** underlyingDecimals;
+        //Update oracle at deployment, to avoid having to check against 0 observations for the rest of the oracle's lifetime
+        _update();
     }
 
     /**
@@ -76,16 +83,15 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     function price0TWAP(uint observationsToDiscard) internal view returns (uint) {
         uint length = observationCount;
-        require(length > 0, 'No length-1 TWAP observation.');//TODO: A lot of checking to do for something that's only relevant when 0 observations have been made
         require(observationsToDiscard < 2);
         Observation memory lastObservation = observations[(length - observationsToDiscard) % OBSERVATION_BUFFER];
         if (lastObservation.timestamp > now - MIN_TWAP_TIME) {
-            require(length > 1, 'No length-2 TWAP observation.');//TODO: A lot of checking to do for something that's only relevant when 0 observations have been made
+            require(length > 1, 'No length-2 TWAP observation.');//TODO: A lot of checking to do for something that's only relevant when only 1 observation have been made
             lastObservation = observations[(length - 1 - observationsToDiscard) % OBSERVATION_BUFFER];
         }
         uint elapsedTime = now - lastObservation.timestamp;
         require(elapsedTime >= MIN_TWAP_TIME, 'Bad TWAP time.');
-        return (currentPx0Cumu() - lastObservation.price0Cumulative) / (now - lastObservation.timestamp); // overflow is desired
+        return (currentPx0Cumu() - lastObservation.price0Cumulative) / elapsedTime; // overflow is desired
     }
 
     /**
@@ -95,16 +101,15 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     function price1TWAP(uint observationsToDiscard) internal view returns (uint) {
         uint length = observationCount;
-        require(length > 0, 'No length-1 TWAP observation.'); //TODO: A lot of checking to do for something that's only relevant when 0 observations have been made
         require(observationsToDiscard < 2);
         Observation memory lastObservation = observations[(length - observationsToDiscard) % OBSERVATION_BUFFER];
         if (lastObservation.timestamp > now - MIN_TWAP_TIME) {
-            require(length > 1, 'No length-2 TWAP observation.');//TODO: A lot of checking to do for something that's only relevant when 0 observations have been made
+            require(length > 1, 'No length-2 TWAP observation.');//TODO: A lot of checking to do for something that's only relevant when only 1 observation have been made
             lastObservation = observations[(length - 1 - observationsToDiscard) % OBSERVATION_BUFFER];
         }
         uint elapsedTime = now - lastObservation.timestamp;
         require(elapsedTime >= MIN_TWAP_TIME, 'Bad TWAP time.');
-        return (currentPx1Cumu() - lastObservation.price1Cumulative) / (now - lastObservation.timestamp); // overflow is desired
+        return (currentPx1Cumu() - lastObservation.price1Cumulative) / elapsedTime; // overflow is desired
     }
 
     /**
@@ -113,8 +118,8 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     function currentPx0Cumu() internal view returns (uint px0Cumu) {
         uint32 currTime = uint32(now);
-        px0Cumu = IUniswapV2Pair(pair).price0CumulativeLast();
-        (uint reserve0, uint reserve1, uint32 lastTime) = IUniswapV2Pair(pair).getReserves();
+        px0Cumu = pair.price0CumulativeLast();
+        (uint reserve0, uint reserve1, uint32 lastTime) = pair.getReserves();
         if (lastTime != now) {
             uint32 timeElapsed = currTime - lastTime; // overflow is desired
             px0Cumu += uint((reserve1 << 112) / reserve0) * timeElapsed; // overflow is desired
@@ -127,8 +132,8 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     function currentPx1Cumu() internal view returns (uint px1Cumu) {
         uint32 currTime = uint32(now);
-        px1Cumu = IUniswapV2Pair(pair).price1CumulativeLast();
-        (uint reserve0, uint reserve1, uint32 lastTime) = IUniswapV2Pair(pair).getReserves();
+        px1Cumu = pair.price1CumulativeLast();
+        (uint reserve0, uint reserve1, uint32 lastTime) = pair.getReserves();
         if (lastTime != currTime) {
             uint32 timeElapsed = currTime - lastTime; // overflow is desired
             px1Cumu += uint((reserve0 << 112) / reserve1) * timeElapsed; // overflow is desired
@@ -140,13 +145,11 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     function price() public view returns (uint) {
         // Return ERC20/ETH TWAP
-        uint256 baseUnit = 10 ** uint256(ERC20Upgradeable(underlying).decimals());
         return (underlying < WETH ? price0TWAP(1) : price1TWAP(1)).div(2 ** 56).mul(baseUnit).div(2 ** 56); // Scaled by 1e18, not 2 ** 112
     }
 
     function observationPrice() internal view returns (uint) {
         // Return ERC20/ETH TWAP
-        uint256 baseUnit = 10 ** uint256(ERC20Upgradeable(underlying).decimals());
         return (underlying < WETH ? price0TWAP(0) : price1TWAP(0)).div(2 ** 56).mul(baseUnit).div(2 ** 56); // Scaled by 1e18, not 2 ** 112
     }
 
@@ -174,36 +177,27 @@ contract UniswapTwapPriceOracleV2Ceiling {
      */
     Observation[OBSERVATION_BUFFER] public observations;
 
-
-    /// @notice Check which of multiple pairs are workable/updatable.
-    function workable( uint256 minPeriod, uint256 deviationThreshold) external view returns (bool) {
-        return _workable(minPeriod, deviationThreshold);
-    }
-    
-    /// @dev Internal function to check if a pair is workable (updateable AND reserves have changed AND deviation threshold is satisfied).
-    function _workable(uint256 minPeriod, uint256 deviationThreshold) internal view returns (bool) {
+    /// @dev Internal function to check if oracle is workable (updateable AND reserves have changed AND deviation threshold is satisfied).
+    function workable(uint256 minPeriod, uint256 deviationThreshold) external view returns (bool) {
         // Workable if:
-        // 1) We have no observations
-        // 2) The elapsed time since the last observation is > minPeriod AND reserves have changed AND deviation threshold is satisfied 
-        // Note that we loop observationCount[pair] around OBSERVATION_BUFFER so we don't waste gas on new storage slots
-        if (observationCount <= 0) return true;
-        (, , uint32 lastTime) = IUniswapV2Pair(pair).getReserves();
+        // The elapsed time since the last observation is > minPeriod AND reserves have changed AND deviation threshold is satisfied 
+        // Note that we loop observationCount around OBSERVATION_BUFFER so we don't waste gas on new storage slots
+        (, , uint32 lastTime) = pair.getReserves();
         return (block.timestamp - observations[(observationCount - 1) % OBSERVATION_BUFFER].timestamp) > (minPeriod >= MIN_TWAP_TIME ? minPeriod : MIN_TWAP_TIME) &&
             lastTime != observations[(observationCount - 1) % OBSERVATION_BUFFER].timestamp &&
             _deviation() >= deviationThreshold;
     }
 
-    /// @dev Internal function to check if a pair's spot price's deviation from its TWAP price as a ratio scaled by 1e18
+    /// @dev Internal function to return oracle deviation from its TWAP price as a ratio scaled by 1e18
     function _deviation() internal view returns (uint256) {
-        // Get token base unit
-        bool useToken0Price = IUniswapV2Pair(pair).token0() != WETH;
-        uint256 baseUnit = 10 ** uint256(ERC20Upgradeable(underlying).decimals());
+        // Figure out which TWAP price to use
+        bool useToken0Price = pair.token0() != WETH;
 
         // Get TWAP price
         uint256 twapPrice = (useToken0Price ? price0TWAP(1) : price1TWAP(1)).div(2 ** 56).mul(baseUnit).div(2 ** 56); // Scaled by 1e18, not 2 ** 112
     
         // Get spot price
-        (uint reserve0, uint reserve1, ) = IUniswapV2Pair(pair).getReserves();
+        (uint reserve0, uint reserve1, ) = pair.getReserves();
         uint256 spotPrice = useToken0Price ? reserve1.mul(baseUnit).div(reserve0) : reserve0.mul(baseUnit).div(reserve1);
 
         // Get ratio and return deviation
@@ -211,39 +205,35 @@ contract UniswapTwapPriceOracleV2Ceiling {
         return ratio >= 1e18 ? ratio - 1e18 : 1e18 - ratio;
     }
     
-    /// @dev Internal function to check if a pair is updatable at all.
+    /// @dev Internal function to check if oracle is updatable at all.
     function _updateable() internal view returns (bool) {
         // Updateable if:
-        // 1) We have no observations
-        // 2) The elapsed time since the last observation is > MIN_TWAP_TIME
-        // Note that we loop observationCount[pair] around OBSERVATION_BUFFER so we don't waste gas on new storage slots
-        if(observationCount <= 0 || (block.timestamp - observations[(observationCount - 1) % OBSERVATION_BUFFER].timestamp) > MIN_TWAP_TIME){
+        // 1) The elapsed time since the last observation is > MIN_TWAP_TIME
+        // 2) The observation price (current price) is below priceCeiling
+        // Note that we loop observationCount around OBSERVATION_BUFFER so we don't waste gas on new storage slots
+        if((block.timestamp - observations[(observationCount - 1) % OBSERVATION_BUFFER].timestamp) > MIN_TWAP_TIME){
             return observationPrice() < priceCeiling;        
-        } else {
-            return false;
         }
+        return false;
     }
 
-    /// @notice Update one pair.
+    /// @notice Update the oracle
     function update() external {
-        require(_update(), "Failed to update pair.");
+        require(_updateable(), "NOT UPDATEABLE");
+        _update();
     }
 
 
-    /// @dev Internal function to update a single pair.
-    function _update() internal returns (bool) {
-        // Check if workable
-        if (!_updateable()) return false;
-
+    /// @dev Internal function to update
+    function _update() internal{
         // Get cumulative price(s)
-        uint256 price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
-        uint256 price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
+        uint256 price0Cumulative = pair.price0CumulativeLast();
+        uint256 price1Cumulative = pair.price1CumulativeLast();
         
-        // Loop observationCount[pair] around OBSERVATION_BUFFER so we don't waste gas on new storage slots
-        (, , uint32 lastTime) = IUniswapV2Pair(pair).getReserves();
+        // Loop observationCount around OBSERVATION_BUFFER so we don't waste gas on new storage slots
+        (, , uint32 lastTime) = pair.getReserves();
         observations[observationCount % OBSERVATION_BUFFER] = Observation(lastTime, price0Cumulative, price1Cumulative);
         observationCount++;
-        return true;
     }
     // **************************
     // **  GUARDIAN FUNCTIONS  **

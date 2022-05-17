2b8149a82c186eba49de771cdc9693de05fdc202
08xmt <markus.08xmt@gmail.com>
2022-05-17 11:24:50 +0200

    Add more tests

--- test/UniswapTwapPriceOracleV2Ceiling.t.sol
+++ test/UniswapTwapPriceOracleV2Ceiling.t.sol
@@ -4,7 +4,7 @@ import {Vm} from "forge-std/Vm.sol";
 import "ds-test/test.sol";
 import "src/UniswapTwapPriceOracleV2Ceiling.sol";
 import "src/IUniswapV2Pair.sol";
-import "IERC20.sol";
+import "src/IERC20.sol";
 
 contract UniswapTwapPriceOracleV2CeilingTests is DSTest {
     Vm internal constant vm = Vm(HEVM_ADDRESS);
@@ -34,36 +34,112 @@ contract UniswapTwapPriceOracleV2CeilingTests is DSTest {
         assertTrue(oracle.update());
     }
 
-    function test_UpdateFalse_When_WithinTWAPTime() public {
+    function test_Update_IsFalse_When_WithinTWAPTime() public {
         vm.startPrank(wethHolder);
+        vm.warp(block.timestamp + 15 minutes + 1);
         assertTrue(oracle.update());
         swapExactIn(1 ether, weth, unipair, wethHolder);
 
+        assert(oracle.update() == true);
+        swapExactIn(1 ether, weth, unipair, wethHolder);
         assert(oracle.update() == false);
         vm.warp(block.timestamp + oracle.MIN_TWAP_TIME() - 1);
+        swapExactIn(1 ether, weth, unipair, wethHolder);
         assert(oracle.update() == false);
+        vm.warp(block.timestamp + 2);
+        swapExactIn(1 ether, weth, unipair, wethHolder);
+        assert(oracle.update() == true);
     }
 
-    function test_PriceChange_When_UpdateIsCalled() public {
+    function test_Price_HasChanged_When_UpdateIsCalled() public {
         vm.warp(block.timestamp + 15 minutes + 1);
         oracle.update();
         vm.startPrank(wethHolder);
         uint priceBefore = oracle.price();
-        log_uint(priceBefore);
 
         swapExactIn(1 ether, weth, unipair, wethHolder);
         vm.warp(block.timestamp + oracle.MIN_TWAP_TIME() +1);
         assertTrue(oracle.update());
-        log_uint(oracle.price());
 
         assert(priceBefore < oracle.price());
+    }
+
+    function test_Price_EqualCeiling_When_PriceHigerThanCeiling() public {
+        vm.warp(block.timestamp + 15 minutes + 1);
+        oracle.update();
+        vm.startPrank(guardian);
+        oracle.setPriceCeiling(oracle.price()*2);
+        vm.stopPrank();
+        vm.startPrank(wethHolder);
+
+        swapExactIn(2000 ether, weth, unipair, wethHolder);
+        vm.warp(block.timestamp + oracle.MIN_TWAP_TIME() +1);
+        assertTrue(oracle.update());
+
+        assertEq(oracle.priceCeiling(), oracle.price());
+       
+    }
+
+    function test_setPriceCeiling_Succeed_When_SetByGuardian() public{
+        vm.startPrank(guardian);
+        vm.warp(block.timestamp + 15 minutes + 1);
+        oracle.update();
+        uint price = oracle.price();
+        uint priceCeilingBefore = oracle.priceCeiling();
+
+        oracle.setPriceCeiling(price*2);
+
+        assertEq(price*2, oracle.priceCeiling());
+        assert(oracle.priceCeiling() != priceCeilingBefore);
+    }
+
+    function testFail_setPriceCeiling_Revert_When_SetAboveBounds() public{
+        vm.startPrank(guardian);
+        vm.warp(block.timestamp + 15 minutes + 1);
+        oracle.update();
+
+        oracle.setPriceCeiling(type(uint).max);
+    }
+
+    function testFail_setPriceCeiling_Revert_When_SetBelowBounds() public{
+        vm.startPrank(guardian);
+        vm.warp(block.timestamp + 15 minutes + 1);
+        oracle.update();
+
+        oracle.setPriceCeiling(oracle.price());
+    }
+
+    function test_setMaxBPCeiling_Succeed_When_SetByGovernance() public {
+        vm.startPrank(gov);
+
+        oracle.setMaxBPCeiling(10000);
+    }
+
+
+    function testFail_setMaxBPCeiling_Fail_When_SetBelowMinBPCeiling() public {
+        vm.startPrank(gov);
+
+        oracle.setMaxBPCeiling(oracle.minBPCeiling()-1);
+    }
+
+    function test_setMinBPCeiling_Succeed_When_SetByGovernance() public {
+        vm.startPrank(gov);
+
+        oracle.setMinBPCeiling(10000);
+    }
+
+    function testFail_setMinBPCeiling_Fail_When_SetAboveMaxBPCeiling() public {
+        vm.startPrank(gov);
+
+        oracle.setMinBPCeiling(oracle.maxBPCeiling()+1);
     }
 
     // ************************
     // * ACCESS CONTROL TESTS *
     // ************************
-
     function testFail_setPriceCeiling_Fail_When_CalledByNonGuardian() public{
+        vm.warp(block.timestamp + 15 minutes + 1);
+        oracle.update();
         vm.startPrank(wethHolder);
         oracle.setPriceCeiling(oracle.price()*2);
     }
